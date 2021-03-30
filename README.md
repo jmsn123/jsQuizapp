9542f170f38d59a559ec7abb19358f4c3c5d7e87
rashawn doyley <rashawndoyley@gmail.com>
2021-03-30 10:04:20 -0400

    added a reademe fixed scores page

---
+++ README.md
@@ -0,0 +1,20 @@
+
+
+ # Welcome to My js quiz app
+
+## 
+        
+* The owner of this repo is jmsn123
+      
+
+        
+### Description: 
+    vanilla js quiz app 
+        
+
+## Todo:
+    add a way to insert your own questions 
+    spruce up ui/ux 
+
+    
+        
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,9 +2,23 @@ const socresList = document.querySelector("#userScore")
 const clearScores = document.querySelector("#clear")
 
 
-let taste = JSON.stringify(localStorage.getItem("scores")) || []
-console.log(JSON.parse(taste))
+let taste = JSON.parse(localStorage.getItem("scores")) || []
+
+clearScores.addEventListener("click", () => {
+    localStorage.clear()
+    taste = []
+    socresList.innerHTML = ""
+    buildScores()
+})
 
 function buildScores() {
+    console.log()
+    taste.map(item => {
+        const newScore = document.createElement("li")
 
+        newScore.textContent = item.name + " " + item.score
+        socresList.append(newScore)
+    })
 }
+
+buildScores()
